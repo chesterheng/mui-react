ad29334a5669c01c671ea8d9049c631be5efd925
Chester Heng <chester.heng@gmail.com>
2020-03-12 06:21:41 +0800

    22.Using Tabs

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -2,7 +2,9 @@ import React, { cloneElement, Fragment } from 'react'
 import { 
   AppBar, 
   Toolbar, 
-  useScrollTrigger
+  useScrollTrigger,
+  Tabs,
+  Tab
 } from '@material-ui/core'
 import { makeStyles } from '@material-ui/styles'
 import logo from '../../assets/logo.svg'
@@ -26,6 +28,14 @@ const useStyles = makeStyles(theme => ({
   },
   logo: {
     height: "7em"
+  },
+  tabContainer: {
+    marginLeft: "auto"
+  },
+  tab: {
+    ...theme.typography.tab,
+    minWidth: 10,
+    marginLeft: "25px"
   }
 }))
 
@@ -38,6 +48,13 @@ const Header = () => {
         <AppBar position="fixed" color="primary">
           <Toolbar disableGutters>
             <img src={logo} alt="company logo" className={classes.logo}/>
+            <Tabs className={classes.tabContainer}>
+              <Tab className={classes.tab} label="Home" />
+              <Tab className={classes.tab} label="Services" />
+              <Tab className={classes.tab} label="The Revolution" />
+              <Tab className={classes.tab} label="About Us" />
+              <Tab className={classes.tab} label="Contact Us" />
+            </Tabs>
           </Toolbar>
         </AppBar>
       </ElevationScroll>
--- src/components/ui/Theme.js
+++ src/components/ui/Theme.js
@@ -17,8 +17,11 @@ export default createMuiTheme({
     }
   },
   typography: {
-    h3: {
-      fontWeight: 300
+    tab: {
+      fontFamily: "Raleway",
+      textTransform: "none",
+      fontWeight: 700,
+      fontSize: "1rem",
     }
   }
 });
